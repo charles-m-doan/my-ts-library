42b2b30bda5d3a4396abbbca220354aaa550e2a4
charles-m-doan <cmatthewdoan@gmail.com>
2023-01-09 10:23:21 -0500

    Adding todo comment.

--- src/app/library/examples/user.service.spec.ts
+++ src/app/library/examples/user.service.spec.ts
@@ -16,6 +16,9 @@ describe('UserService', () => {
     console.log('parent beforeEach: ' + parent);
     TestBed.configureTestingModule({
       providers: [
+        // TODO: Modify createMockProvider so that it is unecessary to supply a behavior subject every time.
+        // And/OR that it will pull the "mockData" from a function that maps Types to mockData - similar to how you began.
+        // Obviously whatever is provided to the createMockProvider function should override whatever gets assigned by default.
         createMockProvider(TransactionService, { teller$: new BehaviorSubject(new Teller()) }),
         createMockProvider(ExampleService)
       ]
